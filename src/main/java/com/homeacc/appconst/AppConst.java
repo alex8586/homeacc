a22734d9e52db8bfc14d2a69a0fdc06ec2d83872
Aleksandr Tocilin <aleksandr.tocilin@gmail.com>
2018-01-25 22:31:27 +0200

    little graphic changes

--- src/main/java/com/homeacc/appconst/AppConst.java
+++ src/main/java/com/homeacc/appconst/AppConst.java
@@ -2,6 +2,6 @@ package com.homeacc.appconst;
 
 public class AppConst {
 
-	public static final int APP_WIDTH = 600;
-	public static final int APP_HEIGHT = 400;
+	public static final int APP_WIDTH = 800;
+	public static final int APP_HEIGHT = 600;
 }
