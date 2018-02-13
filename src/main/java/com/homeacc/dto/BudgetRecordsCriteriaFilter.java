cbb2779dd3dcf9c3e52e6551bf12e26fcf124756
Aleksandr Tocilin <aleksandr.tocilin@gmail.com>
2018-02-13 23:42:38 +0200

    added BudgetRecordsCriteriaFilter and builder

---
+++ src/main/java/com/homeacc/dto/BudgetRecordsCriteriaFilter.java
@@ -0,0 +1,84 @@
+package com.homeacc.dto;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import com.homeacc.entity.BudgetType;
+import com.homeacc.entity.Category;
+import com.homeacc.entity.Users;
+
+public class BudgetRecordsCriteriaFilter {
+
+	private Users user;
+	private Category category;
+	private BudgetType budgetType;
+	private Date dateFrom;
+	private Date dateTo;
+	private BigDecimal amountFrom;
+	private BigDecimal amountTo;
+
+	public Users getUser() {
+		return user;
+	}
+	public void setUser(Users user) {
+		this.user = user;
+	}
+	public Category getCategory() {
+		return category;
+	}
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	public BudgetType getBudgetType() {
+		return budgetType;
+	}
+	public void setBudgetType(BudgetType budgetType) {
+		this.budgetType = budgetType;
+	}
+	public Date getDateFrom() {
+		return dateFrom;
+	}
+	public void setDateFrom(Date dateFrom) {
+		this.dateFrom = dateFrom;
+	}
+	public Date getDateTo() {
+		return dateTo;
+	}
+	public void setDateTo(Date dateTo) {
+		this.dateTo = dateTo;
+	}
+	public BigDecimal getAmountFrom() {
+		return amountFrom;
+	}
+	public void setAmountFrom(BigDecimal amountFrom) {
+		this.amountFrom = amountFrom;
+	}
+	public BigDecimal getAmountTo() {
+		return amountTo;
+	}
+	public void setAmountTo(BigDecimal amountTo) {
+		this.amountTo = amountTo;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("FilterBudgetRecordsCriteria [user=");
+		builder.append(user);
+		builder.append(", category=");
+		builder.append(category);
+		builder.append(", budgetType=");
+		builder.append(budgetType);
+		builder.append(", dateFrom=");
+		builder.append(dateFrom);
+		builder.append(", dateTo=");
+		builder.append(dateTo);
+		builder.append(", amountFrom=");
+		builder.append(amountFrom);
+		builder.append(", amountTo=");
+		builder.append(amountTo);
+		builder.append("]");
+		return builder.toString();
+	}
+
+}
---
+++ src/main/java/com/homeacc/dto/BudgetRecordsCriteriaFilterBuilder.java
@@ -0,0 +1,75 @@
+package com.homeacc.dto;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import com.homeacc.entity.BudgetType;
+import com.homeacc.entity.Category;
+import com.homeacc.entity.Users;
+
+public class BudgetRecordsCriteriaFilterBuilder {
+
+	private Users user;
+	private Category category;
+	private BudgetType budgetType;
+	private Date dateFrom;
+	private Date dateTo;
+	private BigDecimal amountFrom;
+	private BigDecimal amountTo;
+
+	public BudgetRecordsCriteriaFilterBuilder() {
+
+	}
+
+	public static BudgetRecordsCriteriaFilterBuilder createBudgetRecordsCriteraFilter() {
+        return new BudgetRecordsCriteriaFilterBuilder();
+	}
+
+	public BudgetRecordsCriteriaFilter build() {
+		BudgetRecordsCriteriaFilter criteria = new BudgetRecordsCriteriaFilter();
+		criteria.setUser(user);
+		criteria.setCategory(category);
+		criteria.setBudgetType(budgetType);
+		criteria.setDateFrom(dateFrom);
+		criteria.setDateTo(dateTo);
+		criteria.setAmountFrom(amountFrom);
+		criteria.setAmountTo(amountTo);
+		return criteria;
+	}
+
+	public BudgetRecordsCriteriaFilterBuilder withUser(Users user) {
+        this.user = user;
+        return this;
+	}
+
+	public BudgetRecordsCriteriaFilterBuilder withCategory(Category category) {
+        this.category = category;
+        return this;
+	}
+
+	public BudgetRecordsCriteriaFilterBuilder withBudgetType(BudgetType budgetType) {
+        this.budgetType = budgetType;
+        return this;
+	}
+
+	public BudgetRecordsCriteriaFilterBuilder withDateFrom(Date dateFrom) {
+        this.dateFrom = dateFrom;
+        return this;
+	}
+
+	public BudgetRecordsCriteriaFilterBuilder withDateTo(Date dateTo) {
+        this.dateTo = dateTo;
+        return this;
+	}
+
+	public BudgetRecordsCriteriaFilterBuilder withAmountFrom(BigDecimal amountFrom) {
+        this.amountFrom = amountFrom;
+        return this;
+	}
+
+	public BudgetRecordsCriteriaFilterBuilder withAmountTo(BigDecimal amountTo) {
+        this.amountTo = amountTo;
+        return this;
+	}
+
+}
